632242176b6cabfe4fbc160fc8995116246db604
Aaron Diable <aaron.diable@gmail.com>
2020-01-20 19:10:00 +0000

    Test one complete

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,6 @@
 feature 'HomePage Works' do
   scenario 'Visiting homepage returns content' do
     visit '/'
-    expect(page).to have_content("Wel3cfdgfdgsdfgome to Chitter")
+    expect(page).to have_content("Welcome to Chitter")
   end
 end
