56bb34839fd9abb9be3462be09afbef33ffc8fd2
Aaron Diable <aaron.diable@gmail.com>
2020-01-20 22:19:31 +0000

    Fourth user story - Creating an account

--- spec/features/US4_creating_a_user_account.rb
+++
@@ -1,6 +0,0 @@
-feature "I want to sign up for Chitter" do
-  it 'Shows the home page' do
-    visit '/home'
-    expect(page).to have_content("Welcome to Chitter")
-  end
-end
---
+++ spec/features/US4_creating_a_user_account_spec.rb
@@ -0,0 +1,18 @@
+feature "I want to sign up for Chitter" do
+  it 'Shows the home page' do
+    visit '/home'
+    expect(page).to have_content("My previous peeps")
+  end
+
+  it 'creates a new user account' do
+    visit '/home'
+    click_link "Create Account"
+    fill_in :email, with: "an_email@future.uk"
+    fill_in :password, with: "shhhdonttell"
+    fill_in :password2, with: "shhhdonttell"
+    fill_in :f_name, with: "Lord"
+    fill_in :l_name, with: "Aaron"
+    click_link "Submit"
+    expect(page).to have_content("Hello Lord Aaron")
+  end
+end
