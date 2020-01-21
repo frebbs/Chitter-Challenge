d4f7780c4d4c54e13e94655bf668af471fad0129
Aaron Diable <aaron.diable@gmail.com>
2020-01-21 00:37:32 +0000

    Some changes

---
+++ spec/user_model_spec.rb
@@ -0,0 +1,12 @@
+require './lib/user_model'
+require './lib/connect_to_db'
+require_relative 'db_helper'
+
+describe User do
+  describe '.create' do
+    it 'creates a new user' do
+      
+    end
+  end
+end
+
